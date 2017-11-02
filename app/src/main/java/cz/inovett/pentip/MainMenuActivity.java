2ed8a0680d6124b758ecd70942b81e01be2da930
Dankyel Cordero <dankielcordero@gmail.com>
2017-11-02 09:59:12 +0100

    05-PostActivity

--- app/src/main/java/cz/inovett/pentip/MainMenuActivity.java
+++ app/src/main/java/cz/inovett/pentip/MainMenuActivity.java
@@ -1,9 +1,9 @@
 package cz.inovett.pentip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,8 +27,8 @@ public class MainMenuActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Open PostActivity
+                startActivity(new Intent(MainMenuActivity.this, PostActivity.class));
             }
         });
 
